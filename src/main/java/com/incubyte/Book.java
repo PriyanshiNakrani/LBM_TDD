096de76f179e835de32e8a44a7b5e77d2d2fc5fa
Priyanshi Nakrani <143703370+PriyanshiNakrani@users.noreply.github.com>
2024-08-11 02:17:48 +0530

    Added passed all test cases for publicaition year validation of constructor call

--- src/main/java/com/incubyte/Book.java
+++ src/main/java/com/incubyte/Book.java
@@ -7,14 +7,14 @@ public class Book {
     private int publicationYear;
 
     public Book(String title, String author, String isbn, int publicationYear) {
-        validateTitleAndAuthor(title, author,isbn);
+        validateTitleAndAuthor(title, author,isbn, publicationYear);
         this.isbn = isbn;
         this.title = title;
         this.author = author;
         this.publicationYear = publicationYear;
     }
 
-    private void validateTitleAndAuthor(String title, String author,String isbn) {
+    private void validateTitleAndAuthor(String title, String author,String isbn,int publicationYear) {
         if (title == null || author == null) {
             throw new IllegalArgumentException("Title and Author cannot be null.");
         }
@@ -32,6 +32,11 @@ public class Book {
 
         if (!isbn.matches("\\d{13}")) {
             throw new IllegalArgumentException("ISBN must be exactly 13 digits long and contain only numeric values.");
+        }
+        String publicationYearStr = String.valueOf(publicationYear);
+
+        if (!publicationYearStr.matches("\\d{4}")) {
+            throw new IllegalArgumentException("Publication year must be exactly 4 digits long and contain only numeric values.");
         }
     }
 }
