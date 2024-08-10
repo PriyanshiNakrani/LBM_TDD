be356caf92ed3dda07708642742e8e8a729a9553
Priyanshi Nakrani <143703370+PriyanshiNakrani@users.noreply.github.com>
2024-08-11 02:36:31 +0530

    Added failed test for testing UserName of constructor call

--- src/main/java/com/incubyte/User.java
+++ src/main/java/com/incubyte/User.java
@@ -1,5 +1,10 @@
 package com.incubyte;
 
 public class User {
-    
+    private String UserName;
+    private int userId;
+
+    private User(){
+        
+    }
 }
--- src/test/java/TestUser.java
+++ src/test/java/TestUser.java
@@ -1,3 +1,28 @@
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import com.incubyte.User;
+
 public class TestUser {
     
+    @Test
+    public void testThat_ConstructorMustThrowInvalidArgumentExceptionWhenUserNameIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> new User(null, 12345));
+    }
+
+    @Test
+    public void testThat_ConstructorMustThrowInvalidArgumentExceptionWhenUserNameContainsNonAlphabeticCharacters() {
+        assertThrows(IllegalArgumentException.class, () -> new User("John123", 12345));
+    }
+
+    @Test
+    public void testThat_ConstructorMustThrowInvalidArgumentExceptionWhenUserNameIsLessThan3Characters() {
+        assertThrows(IllegalArgumentException.class, () -> new User("Jo", 12345));
+    }
+
+    @Test
+    public void testThat_ConstructorMustThrowInvalidArgumentExceptionWhenUserNameStartsWithNumericValue() {
+        assertThrows(IllegalArgumentException.class, () -> new User("1John", 12345));
+    }
 }
