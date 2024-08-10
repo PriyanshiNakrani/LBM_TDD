61f2cec5c99ae27d4fb54035c6d2cae4e4e669c8
Priyanshi Nakrani <143703370+PriyanshiNakrani@users.noreply.github.com>
2024-08-11 03:13:03 +0530

    Added passed all test cases for Library Name validation of constructor call

--- src/main/java/com/incubyte/Library.java
+++ src/main/java/com/incubyte/Library.java
@@ -3,5 +3,26 @@ package com.incubyte;
 public class Library {
     private String libName;
 
-   public Library(){};
+    public Library(String libName) {
+        validateLibName(libName);
+        this.libName = libName;
+    }
+
+    private void validateLibName(String libName) {
+        if (libName == null) {
+            throw new IllegalArgumentException("Library name cannot be null.");
+        }
+
+        if (libName.length() <= 3) {
+            throw new IllegalArgumentException("Library name must be longer than 3 characters.");
+        }
+
+        if (libName.matches("^[0-9].*")) {
+            throw new IllegalArgumentException("Library name cannot start with a numeric value.");
+        }
+
+        if (libName.matches(".*\\d.*")) {
+            throw new IllegalArgumentException("Library name cannot contain numeric values.");
+        }
+    }
 }
