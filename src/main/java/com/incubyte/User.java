cbeb49ead192692421cd7445a12ae8a754c309d0
Priyanshi Nakrani <143703370+PriyanshiNakrani@users.noreply.github.com>
2024-08-11 02:44:33 +0530

    Added passed all test cases for UserName validation of constructor call

--- src/main/java/com/incubyte/User.java
+++ src/main/java/com/incubyte/User.java
@@ -2,9 +2,29 @@ package com.incubyte;
 
 public class User {
     private String UserName;
-    private int userId;
+    private int UserId;
 
-    private User(){
-        
+    public User(String UserName, int UserId) {
+        validateUserName(UserName);
+        this.UserName = UserName;
+        this.UserId = UserId;
+    }
+
+    private void validateUserName(String UserName) {
+        if (UserName == null) {
+            throw new IllegalArgumentException("UserName cannot be null.");
+        }
+
+        if (UserName.length() <= 3) {
+            throw new IllegalArgumentException("UserName must be longer than 3 characters.");
+        }
+
+        if (!UserName.matches("^[a-zA-Z].*")) {
+            throw new IllegalArgumentException("UserName cannot start with a numeric value.");
+        }
+
+        if (!UserName.matches("^[a-zA-Z]+$")) {
+            throw new IllegalArgumentException("UserName can only contain alphabetic characters.");
+        }
     }
 }
